2f5bd4e5a598891d910c2ed929ba27c25e317ef4
ravi <ravish1086@gmail.com>
2026-04-01 15:14:26 +0530

    feat: implement generated invoice component with print and display toggle functionality

--- src/app/generatedinvoice/generatedinvoice.component.ts
+++ src/app/generatedinvoice/generatedinvoice.component.ts
@@ -31,7 +31,7 @@ export class GeneratedinvoiceComponent implements OnInit {
   houseNum: any;
   area: any;
   pan: any;
-  balancerow: number = 25;
+  balancerow: number = 22;
   balancerows: any[] = [];
   invoiceNum: any;
   invoiceType: string = "Original";
